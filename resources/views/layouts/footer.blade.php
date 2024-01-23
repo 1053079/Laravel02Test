2966d8fc03f6531a8deb717b82f4786c9aa8bc18
Yong Pok Low <yongpok95@live.nl>
2024-01-23 18:46:16 +0100

    add css + home page

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href={{asset('assets/css/footer.css')}}>
+
+<footer class="footer">
+    <p>
+        Contact us
+    </p>
+    <p>
+        About Us
+    </p>
+</footer>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield("title")</title>
+    {{-- import css down here--}}
+    <link rel="stylesheet" href= {{asset('assets/css/header.css')}}>
+</head>
+
+<header class="header">
+    <nav>
+        <a href="/">
+        <img class ="logo" src="https://upload.wikimedia.org/wikipedia/commons/2/22/Blue_Lock_Logo.png">
+        </a>
+    </nav>
+</header>
+<body>
+    
+</body>
+</html>
